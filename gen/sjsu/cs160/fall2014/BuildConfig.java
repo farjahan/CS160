aecf190bd7d33f6bfb93af822e89a26f6be9aa7c
farjahan <farjahan@mirtanvir.com>
2014-11-14 11:38:09 -0800

    initial setup
    initial setup

---
+++ gen/sjsu/cs160/fall2014/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package sjsu.cs160.fall2014;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/sjsu/cs160/fall2014/MYDB.java
@@ -0,0 +1,5 @@
+package sjsu.cs160.fall2014;
+
+public class MYDB {
+
+}
